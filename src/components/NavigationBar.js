8938ec7e4c1bf30447f22d55fb79766ed8386d75
unknown <victoria.calderon2502@gmail.com>
2023-06-23 20:53:51 -0600

    Actualización Header

---
+++ src/components/NavigationBar.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const NavigationBar = () => {
+  return (
+    <div className="navigation-bar">
+      {/* Contenido de la barra de navegación */}
+    </div>
+  );
+};
+
+export default NavigationBar;
