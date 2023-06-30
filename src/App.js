f1ebd5b760825dd0290161612d34992e75cfa6c2
unknown <victoria.calderon2502@gmail.com>
2023-06-30 14:18:09 -0600

    Committing changes to src/App.js before merge

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
-
 import Navbar from "./components/Navbar";
 import Header from "./components/Header";
 import Category from "./components/Category";
 import Carousel from "./components/Carousel";
+
+//import DisneyGallery from "./components/DisneyGallery";
 import React from "react";
 import "./App.css";
-
 
 
 function App() {
   return (
     <div className="landing-page">
       <Navbar />
-      <Category/>
+      <Category />
       <Header />
-      <Carousel/>
-     
-     
-      
-    
-      
-     
+      <Carousel />
+
+
       <div className="content">{/* Contenido principal de la página */}</div>
     </div>
   );
