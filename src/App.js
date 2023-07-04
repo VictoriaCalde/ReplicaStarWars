582830ef67476defbd416a2e1300679c20087630
unknown <victoria.calderon2502@gmail.com>
2023-07-04 04:08:41 -0600

    Avance tercer carousel

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
 import Navbar from "./components/Navbar";
-import Header from "./components/Header";
 import Category from "./components/Category";
 import Carousel from "./components/Carousel";
+import Carousel3 from "./components/Carousel3";
 
 //import DisneyGallery from "./components/DisneyGallery";
 import React from "react";
 import "./App.css";
 import Footer from "./components/Footer";
 
-
 function App() {
   return (
     <div className="landing-page">
       <Navbar />
       <Category />
-      <Header />
       <Carousel />
+      <Carousel3 />
       <Footer />
 
-      
-    
-      
       <div className="content">{/* Contenido principal de la página */}</div>
     </div>
   );
--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -9,8 +9,16 @@ import imagen4 from "../components/imagenes/disney-gallery-the-mandalorian-s3-mo
 
 const CarouselComponent = () => {
   return (
-    <div className="carousel-container">
+    <div className="carousel1-container">
       <div className="carousel-wrapper">
+        <div className="carousel1-header">
+          <div className="title1-container">
+            <div className="header1-line"></div>
+            <span className="title1-text">
+              ALL OF YOUR STAR WARS FAVORITES NOW STREAMING ON DISNEY+
+            </span>
+          </div>
+        </div>
         <Carousel
           showThumbs={true}
           showStatus={false}
@@ -136,81 +144,95 @@ const CarouselComponent = () => {
           <div>
             <img src={imagen3} alt="Imagen 3" />
             <div className="carousel-overlay overlay-image3">
-             
-                <div className="content-title">
-                  <h2>
-                    <p>Marvel Will Bring</p>
-                    <p>the <i>Obi-Wan Kenobi</i></p>
-                    <p>Disney+ Series</p> 
-                    <p>to Comics</p> 
-                  </h2>
-                </div>
-                <div className="content-description" tabIndex="0">
+              <div className="content-title">
+                <h2>
+                  <p>Marvel Will Bring</p>
                   <p>
-                    <p>Check out the covers of issue #1 and meet</p>
-                    <p>the creative team.</p>
+                    the <i>Obi-Wan Kenobi</i>
                   </p>
-                </div>
-                <p
-                  className="cta-links-container text-left"
-                  data-cta-type="button"
-                >
-                  <span className="cta-item" data-title="Read More">
-                    <span className="btn-container">
-                      <a
-                        href="https://www.starwars.com/news/marvel-star-wars-obi-wan-kenobi"
-                        className="button large blue cta-link ada-el-focus"
-                        role="button"
-                        tabIndex="0"
-                      >
-                        Read More
-                      </a>
-                    </span>
-                  </span>
+                  <p>Disney+ Series</p>
+                  <p>to Comics</p>
+                </h2>
+              </div>
+              <div className="content-description" tabIndex="0">
+                <p>
+                  <p>Check out the covers of issue #1 and meet</p>
+                  <p>the creative team.</p>
                 </p>
-              
+              </div>
+              <p
+                className="cta-links-container text-left"
+                data-cta-type="button"
+              >
+                <span className="cta-item" data-title="Read More">
+                  <span className="btn-container">
+                    <a
+                      href="https://www.starwars.com/news/marvel-star-wars-obi-wan-kenobi"
+                      className="button large blue cta-link ada-el-focus"
+                      role="button"
+                      tabIndex="0"
+                    >
+                      Read More
+                    </a>
+                  </span>
+                </span>
+              </p>
             </div>
           </div>
 
           <div>
-  <img src={imagen4} alt="Imagen 4" />
-  <div className="carousel-overlay overlay-image4">
-    <div className="content-details" style={{ maxWidth: "36%" }}>
-      <div className="content-title">
-        <h1>
-          <img
-            className="logo-title"
-            width="800"
-            height="280"
-            data-src="https://lumiere-a.akamaihd.net/v1/images/disney-gallery-the-mandalorian-s3-module-hero-logo_4bb32455.png?region=0,0,800,280"
-            alt="Disney Gallery: The Mandalorian logo"
-            tabIndex="0"
-            style={{ width: "400px" }}
-            data-width="800"
-            data-height="280"
-            data-aspect_ratio="0.35"
-            data-orientation="landscape"
-            data-legacy="true"
-            src="https://lumiere-a.akamaihd.net/v1/images/disney-gallery-the-mandalorian-s3-module-hero-logo_4bb32455.png?region=0,0,800,280"
-          />
-        </h1>
-      </div>
-      <div className="content-description" tabIndex="0">
-        <p><p>Go behind the scenes in the all-new</p> <p>special <i>Disney Gallery: The Mandalorian</i>,</p> <p>streaming June 28 on Disney+.</p></p>
-      </div>
-      <p className="cta-links-container text-center" data-cta-type="button">
-        <span className="cta-item" data-title="Watch Clip">
-          <span className="btn-container">
-            <a href="https://youtu.be/KVnTWuVPPkM" className="button large blue cta-link ada-el-focus youtube-url" role="button" data-youtube-id="KVnTWuVPPkM" tabIndex="0">
-              Watch Clip
-            </a>
-          </span>
-        </span>
-      </p>
-    </div>
-  </div>
-</div>
-
+            <img src={imagen4} alt="Imagen 4" />
+            <div className="carousel-overlay overlay-image4">
+              <div className="content-details" style={{ maxWidth: "36%" }}>
+                <div className="content-title">
+                  <h1>
+                    <img
+                      className="logo-title"
+                      width="800"
+                      height="280"
+                      data-src="https://lumiere-a.akamaihd.net/v1/images/disney-gallery-the-mandalorian-s3-module-hero-logo_4bb32455.png?region=0,0,800,280"
+                      alt="Disney Gallery: The Mandalorian logo"
+                      tabIndex="0"
+                      style={{ width: "400px" }}
+                      data-width="800"
+                      data-height="280"
+                      data-aspect_ratio="0.35"
+                      data-orientation="landscape"
+                      data-legacy="true"
+                      src="https://lumiere-a.akamaihd.net/v1/images/disney-gallery-the-mandalorian-s3-module-hero-logo_4bb32455.png?region=0,0,800,280"
+                    />
+                  </h1>
+                </div>
+                <div className="content-description" tabIndex="0">
+                  <p>
+                    <p>Go behind the scenes in the all-new</p>{" "}
+                    <p>
+                      special <i>Disney Gallery: The Mandalorian</i>,
+                    </p>{" "}
+                    <p>streaming June 28 on Disney+.</p>
+                  </p>
+                </div>
+                <p
+                  className="cta-links-container text-center"
+                  data-cta-type="button"
+                >
+                  <span className="cta-item" data-title="Watch Clip">
+                    <span className="btn-container">
+                      <a
+                        href="https://youtu.be/KVnTWuVPPkM"
+                        className="button large blue cta-link ada-el-focus youtube-url"
+                        role="button"
+                        data-youtube-id="KVnTWuVPPkM"
+                        tabIndex="0"
+                      >
+                        Watch Clip
+                      </a>
+                    </span>
+                  </span>
+                </p>
+              </div>
+            </div>
+          </div>
         </Carousel>
       </div>
     </div>
